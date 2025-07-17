a030f4451e85669d683dacc0f563ca88d9142a01
ammonanderson64 <and21110@byui.edu>
2025-07-16 19:58:36 -0600

    Update and rename 2-StudentsProblem to 2-StudentsProblem.cs

---
+++ CsharpFundementals/Tree(BinarySearchTree)/2-StudentsProblem.cs
@@ -0,0 +1,9 @@
+//Write a program that:
+//- Inserts the following numbers into a BST: 15, 10, 20, 8, 12, 17, 25
+//- Searches for the number 12
+//- Prints all numbers in the BST in sorted order
+
+//Try modifying the tree to delete a value and test the in-order traversal again.
+
+
+
